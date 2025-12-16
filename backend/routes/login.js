8d41758ce5e741ac798288b685bbce73c6a7bfc9
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-12-16 09:50:51 -0500

    updated login routes

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,13 +1,17 @@
 import express from "express";
 
 const router = express.Router();
-// import { auth } from "../middleware/auth.js";
-
-import loginController from "../controllers/login.js";
+import { auth } from "../middleware/auth.js";
+import cLoginController from "../controllers/login.js";
 
 router
 .route('/')
-.post(loginController.customerLogin)
-// .get(loginController.customerLogin)
+.post(cLoginController.customerLogin)
+
+router
+.route('/employee')
+.post(cLoginController.employeeLogin)
+.get(auth, cLoginController.userLogged)
+
 
 export default router
