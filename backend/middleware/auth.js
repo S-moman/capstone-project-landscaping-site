715a3dd3f511df47a60a353753f36d2ca236e75c
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-12-16 09:40:44 -0500

    update to middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,7 @@
-import Customers from "../models/Customers.js";
-import { jwtDecode } from "jwt-decode";
+// import Customers from "../models/Customers.js";
+import Employees from "../models/Employees.js";
+import jwtDecode from "jsonwebtoken";
+
 
 export const auth = async (req, res, next) => {
   let token;
@@ -8,13 +10,20 @@ export const auth = async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    try {
 
     token = req.headers.authorization.split(" ")[1];
 
     const decode = jwtDecode.verify(token, process.env.JWT_SECRET);
 
-    req.user = await Customers.findById(decode.id).select("-password");
+    req.user = await Employees.findById(decode.id).select("-password");
+
 
     return next();
+    } catch (e) {
+      console.error(e);
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
   }
+  res.status(401).json({ message: "Not authorized, token failed" });
 };
